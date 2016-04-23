cf987bb6418666794ec38b287731f1021f4ef6ad
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2016-04-23 18:41:26 -0300

    Mudança no AlunoDAO
    Colocado o metodo construtor no AlunoDAO
    Inserindo uma lista
    Já é possivel mostrar os alunos cadastrados
    Foi feito um teste em Principal

--- src/com/qst1/dao/AlunoDAO.java
+++ src/com/qst1/dao/AlunoDAO.java
@@ -3,10 +3,17 @@ package com.qst1.dao;
 import java.util.ArrayList;
 
 import com.qst1.vo.Aluno;
+import com.recursos.InOut;
 
 public class AlunoDAO implements DAO {
-	private static ArrayList<Aluno> listaAluno = new ArrayList<Aluno>();
-
+	private ArrayList<Aluno> listaAluno;
+	private String msg = "";
+	private int pos = 0;
+	
+	public AlunoDAO(){
+		listaAluno = new ArrayList<Aluno>();
+	}
+	
 	@Override
 	public <T> void Create(T o) {
 		listaAluno.add((Aluno) o);
@@ -15,8 +22,14 @@ public class AlunoDAO implements DAO {
 
 	@Override
 	public void Show() {
-		// TODO Auto-generated method stub
-		
+		msg = "";
+		for(Aluno aluno : listaAluno){
+			msg += "\nMatricula: " + aluno.getMatricula()+
+					"\nNome: " + aluno.getNome()+
+					"\nCPF: " + aluno.getCPF()+
+					"\n------------------------------------";
+		}
+		InOut.OutMessage(msg);
 	}
 	
 	@Override
--- src/com/qts1/testes/Principal.java
+++ src/com/qts1/testes/Principal.java
@@ -1,12 +1,19 @@
 package com.qts1.testes;
 
+import com.qst1.dao.AlunoDAO;
 import com.qst1.vo.Aluno;
 
 public class Principal {
 	public static void main(String[] args){
+		AlunoDAO lista = new AlunoDAO();
+		
 		Aluno al1 = new Aluno("Breno", "01234567890");
-		Aluno al2 = new Aluno("Bruno", "09876543210");
-		//al1.GradeEscolar(listaDisciplinas);
+		Aluno al2 = new Aluno("Mauricio", "09876543210");
+		
+		lista.Create(al1);
+		lista.Create(al2);
+		
+		lista.Show();
 		
 	}
 }
