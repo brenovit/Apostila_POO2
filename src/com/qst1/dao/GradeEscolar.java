bcf1dd22a8c726f25fd82c6154e5a28a8b81a21e
Breno Nunes <breno.arantes97@gmail.com>
2016-04-23 19:59:37 -0300

    Alteração nas Classes GradeEscolar e Disciplina
    Adicionado o metodo e atributos de codigo das Disciplinas
    Adicionado o metodo de adição de Disciplina

--- src/com/qst1/dao/GradeEscolar.java
+++ src/com/qst1/dao/GradeEscolar.java
@@ -2,13 +2,52 @@ package com.qst1.dao;
 
 import java.util.ArrayList;
 
+import com.qst1.vo.Aluno;
 import com.qst1.vo.Disciplina;
+import com.recursos.InOut;
 
 public class GradeEscolar {
-	ArrayList<Disciplina> listDisc = new ArrayList<Disciplina>();
-	
-	public void AdicionarDiciplina(Disciplina disciplina){
-		listDisc.add(disciplina);
+	private ArrayList<Disciplina> listaDisc;
+	private String msg = ""; 
+
+	public GradeEscolar(){
+		listaDisc = new ArrayList<Disciplina>();	
 	}
 	
+	public void AdicionarDiciplina(Disciplina disciplina){
+		listaDisc.add(disciplina);
+	}
+
+	public void Show() {
+		msg = "Codigo\t-\tNome\n";
+		for(Disciplina disciplina : listaDisc){
+			msg += disciplina.getCodigo()+"\t-\t"+disciplina.getNome() + "\n";
+		}
+		InOut.OutMessage(msg);
+	}
+
+	public int Find(Disciplina disc) {
+		int pos = -1;
+		int posAux = 0;
+		while((posAux < listaDisc.size()) && (!listaDisc.get(posAux).getCodigo().equals(disc.getNome()))){
+			posAux++;
+		}
+		if((posAux < listaDisc.size()) && (!listaDisc.get(posAux).getCodigo().equals(disc.getNome())) == true){
+			pos = posAux;
+		}
+		return pos;
+	}
+
+	public <T> void Delete(T o) {
+		/*int pos = Procurar(o);
+		if(pos != -1){
+			listaDisc.remove(pos);
+		}else{
+			InOut.OutMessage("Cliente não Cadastrado");
+		}	*/
+	}	
+	
+	public void CadastrarGrade(Aluno aluno, Disciplina disc){
+				
+	}
 }
--- src/com/qst1/vo/Disciplina.java
+++ src/com/qst1/vo/Disciplina.java
@@ -4,6 +4,18 @@ public class Disciplina {
 	private String nome;
 	private boolean aprovado;
 	private Float nota;
+	private Integer codigo;
+	
+	private static Integer geradorCodigo = 0;
+	
+	public Disciplina(){
+		geradorCodigo++;
+	}
+	
+	public Disciplina(String nome){
+		geradorCodigo++;
+		this.nome = nome;
+	}
 	
 	public String getNome() {
 		return nome;
@@ -16,6 +28,7 @@ public class Disciplina {
 	public float getNota(){
 		return this.nota;
 	}
+	
 	public void setNota(float valor){
 		this.nota = valor;
 		if(this.nota < 6){
@@ -27,5 +40,13 @@ public class Disciplina {
 	
 	public boolean getAprovado(){
 		return this.aprovado;
+	}
+	
+	public Integer getCodigo(){
+		return this.codigo;
+	}
+	
+	public void setCodigo(Integer codigo){
+		this.codigo = codigo;
 	}
 }
